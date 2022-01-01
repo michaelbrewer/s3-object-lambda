91547d1e0601b525f34ba4fec81f0535ba0228cd
Michael Brewer <michael.brewer@gyft.com>
2021-12-31 21:46:36 -0800

    chore: include event_source decorator

--- src/app.py
+++ src/app.py
@@ -1,20 +1,22 @@
 import boto3
 from urllib.request import urlopen
 
-from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.logging.correlation_paths import S3_OBJECT_LAMBDA
+from aws_lambda_powertools.utilities.data_classes import event_source
 from aws_lambda_powertools.utilities.data_classes.s3_object_event import (
     S3ObjectLambdaEvent,
 )
 
 logger = Logger()
+tracer = Tracer()
 session = boto3.Session()
 s3 = session.client("s3")
 
 
-@logger.inject_lambda_context(correlation_id_path=S3_OBJECT_LAMBDA, log_event=True)
+@logger.inject_lambda_context(correlation_id_path=S3_OBJECT_LAMBDA)
+@event_source(data_class=S3ObjectLambdaEvent)
 def lambda_handler(event, context):
-    event = S3ObjectLambdaEvent(event)
 
     # Get object from S3
     original_object = urlopen(event.input_s3_url).read()
